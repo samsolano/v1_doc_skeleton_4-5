eda239f6a0404c0a8e6e9125de005925d277f6f8
samsolano <samsolano@Sams-MBP-58.lan>
2025-04-05 15:29:32 -0700

    added Recharge doc

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -16,7 +16,12 @@ const Sidebar = () => {
       <div className="flex flex-col h-full">
         <nav className="mt-4">
           <ul className="space-y-2">
-          </ul>
+                      <li>
+              <Link to="/api-docs/recharge" className="block px-4 py-2 text-codium-foreground hover:bg-codium-border">
+                /Recharge
+              </Link>
+            </li>
+</ul>
         </nav>
       </div>
     </aside>
