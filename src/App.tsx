f773fbf75a9e33b32ee8aadc1e7def89914ce70b
Sam Solano <samsolano@Sams-MBP-58.lan>
2025-04-05 15:33:28 -0700

    fresh skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Recharge from "./pages/api-docs/Recharge";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -78,8 +77,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
 
           {/* Created Docs */}
-                  <Route path="/api-docs/recharge" element={<Recharge />} />
-</Routes>
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -16,12 +16,8 @@ const Sidebar = () => {
       <div className="flex flex-col h-full">
         <nav className="mt-4">
           <ul className="space-y-2">
-                      <li>
-              <Link to="/api-docs/recharge" className="block px-4 py-2 text-codium-foreground hover:bg-codium-border">
-                /Recharge
-              </Link>
-            </li>
-</ul>
+
+          </ul>
         </nav>
       </div>
     </aside>
