c0a6d77fa6e7f82535e35b93c7b9391f54efc455
samsolano <samsolano@Sams-MBP-58.lan>
2025-04-05 15:19:17 -0700

    added Recharge doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Recharge from "./pages/api-docs/Recharge";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -75,7 +76,8 @@ const App = () => (
           <Route path="/features/context-awareness" element={<Layout><FeaturesContextAwareness /></Layout>} />
           
           <Route path="*" element={<NotFound />} />
-        </Routes>
+                  <Route path="/api-docs/recharge" element={<Recharge />} />
+</Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
